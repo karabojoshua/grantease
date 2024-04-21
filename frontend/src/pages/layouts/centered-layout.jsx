fadf2973d54baf87722288f7f3eeb404c077858d
TheNumbered <160495350+TheNumbered@users.noreply.github.com>
2024-04-21 22:10:20 +0200

    deleting redundancy

--- frontend/src/pages/layouts/centered-layout.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const CenteredLayout = ({ children }) => {
-  return <div className="centered-layout">{children}</div>;
-};
--- frontend/src/pages/layouts/main-layout.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const MainLayout = ({ children }) => {
-  return <div className="main-layout">{children}</div>;
-};
