b7c2b11e40f4bd4a990c45fe228cb848792f06fa
karabojoshua <2580661@students.wits.ac.za>
2024-04-23 12:27:25 +0000

    added dashboard

---
+++ frontend/src/components/DashboardRoleChangeReq.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EnhancedTable from "./RoleChangeTable.";
+
+export default function RoleChangeRequest() {
+    return (
+        <EnhancedTable/>
+    );
+}
---
+++ frontend/src/pages/admin-dashboard/Dashboard.jsx
@@ -0,0 +1,23 @@
+//@ts-ignore
+import PrimarySearchAppBar from "../../components/AppBar";
+//@ts-ignore
+import DashboardTabs from "../../components/DashboardTabs";
+import './dashboard-styles.css';
+
+function Dashboard() {
+  return (
+    <div>
+        <PrimarySearchAppBar />
+        <main>
+            <section className="banner-area">
+                <h2>Admin Dashboard</h2>
+                <p>Welcome back!</p>
+            </section>
+            <DashboardTabs />
+        </main>
+        
+    </div>
+  );
+}
+
+export default Dashboard;
